a28dee1fa9e9c5a605d14e0b2217a30d17a06a6b
Yohann <yzpt.de@gmail.com>
2023-11-01 10:13:04 +0100

    -

---
+++ img/screens/resize.py
@@ -0,0 +1,13 @@
+import os
+from PIL import Image
+
+def resize_images():
+    for filename in os.listdir('./img/screens/'):
+        if filename.endswith('.jpg') or filename.endswith('.jpeg'):
+            img = Image.open(filename)
+            wpercent = (150/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((150,hsize), Image.ANTIALIAS)
+            img.save(filename)
+
+resize_images()
